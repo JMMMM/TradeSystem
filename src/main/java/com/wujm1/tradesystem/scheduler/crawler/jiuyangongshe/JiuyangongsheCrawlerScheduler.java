b3071bd075a2e96501128e4663e4bb85b41a9567
wujm1 <wujm1@xiaopeng.com>
2024-11-28 17:15:19 +0800

    兜底重试

---
+++ src/main/java/com/wujm1/tradesystem/scheduler/crawler/jiuyangongshe/JiuyangongsheCrawlerScheduler.java
@@ -0,0 +1,30 @@
+package com.wujm1.tradesystem.scheduler.crawler.jiuyangongshe;
+
+import com.wujm1.tradesystem.crawler.jiuyangongshe.JiuyangongsheCrawler;
+import com.wujm1.tradesystem.utils.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author wujiaming
+ * @date 2024-11-28 17:13
+ */
+@Component
+@Slf4j
+public class JiuyangongsheCrawlerScheduler {
+
+    @Autowired
+    private JiuyangongsheCrawler jiuyangongsheCrawler;
+
+    @Scheduled(cron = "0 0 15 45 * ?")
+    public void run() {
+        String today = DateUtils.getDateStr(new Date(), "yyyyMMdd");
+        log.info("爬取韭菜公社交易数据开始");
+        jiuyangongsheCrawler.initJiuyangongshe(today);
+        log.info("爬取韭菜公社交易数据结束");
+    }
+}
