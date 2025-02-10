3e327ccc9f43fa102377abb286610aab7fdec512
wujiaming <289451937@qq.com>
2025-02-10 22:59:24 +0800

    修复漏洞

--- src/main/java/com/wujm1/tradesystem/scheduler/crawler/kaipanla/KaipanlaConceptsCrawlerScheduler.java
+++ src/main/java/com/wujm1/tradesystem/scheduler/crawler/kaipanla/KaipanlaConceptsCrawlerScheduler.java
@@ -2,6 +2,8 @@ package com.wujm1.tradesystem.scheduler.crawler.kaipanla;
 
 import com.wujm1.tradesystem.crawler.jiuyangongshe.JiuyangongsheCrawler;
 import com.wujm1.tradesystem.crawler.kpl.KaipanlaConceptsCrawler;
+import com.wujm1.tradesystem.entity.Stock;
+import com.wujm1.tradesystem.mapper.ext.StockMapperExt;
 import com.wujm1.tradesystem.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author wujiaming
@@ -17,14 +20,22 @@ import java.util.Date;
 @Component
 @Slf4j
 public class KaipanlaConceptsCrawlerScheduler {
+    
     @Autowired
     private KaipanlaConceptsCrawler kaipanlaConceptsCrawler;
-
+    
+    @Autowired
+    private StockMapperExt stockMapperExt;
+    
     @Scheduled(cron = "0 0 23 * * ?")
     public void run() {
         String today = DateUtils.getDateStr(new Date(), "yyyyMMdd");
-        log.info("爬取开盘啦概念开始");
-        kaipanlaConceptsCrawler.initKaipanlaConcepts(today);
-        log.info("爬取开盘啦概念结束");
+        List<Stock> stocks = stockMapperExt.queryStockByDates(null, today, today);
+        for (Stock stock : stocks) {
+            log.info("爬取开盘啦概念开始");
+            kaipanlaConceptsCrawler.initKaipanlaConcepts(stock.getCode());
+            log.info("爬取开盘啦概念结束");
+        }
+        
     }
 }
