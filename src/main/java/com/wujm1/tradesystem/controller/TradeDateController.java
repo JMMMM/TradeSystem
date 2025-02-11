c93cb38ccc70225495719f883b47585bca027f08
JMMMM <289451937@qq.com>
2025-02-11 23:04:33 +0800

    合并请求

--- src/main/java/com/wujm1/tradesystem/controller/TradeDateController.java
+++ src/main/java/com/wujm1/tradesystem/controller/TradeDateController.java
@@ -21,21 +21,21 @@ import java.util.List;
 @RequestMapping("/tradeDate")
 @RestController
 public class TradeDateController {
-    
+
     private final TradeDateCrawler tradeDateCrawler;
-    
+
     private final WencaiConditionCrawler wencaiConditionCrawler;
-    
+
     private final EmotionCrawler emotionCrawler;
-    
+
     private final JiuyangongsheCrawler jiuyangongsheCrawler;
-    
+
     private final KaipanlaTdCrawler kaipanlaTdCrawler;
-    
+
     private final KaipanlaConceptsCrawler kaipanlaConceptsCrawler;
-    
+
     public TradeDateController(TradeDateCrawler tradeDateCrawler, WencaiConditionCrawler wencaiConditionCrawler, EmotionCrawler emotionCrawler, JiuyangongsheCrawler jiuyangongsheCrawler,
-            KaipanlaTdCrawler kaipanlaTdCrawler, KaipanlaConceptsCrawler kaipanlaConceptsCrawler) {
+                               KaipanlaTdCrawler kaipanlaTdCrawler, KaipanlaConceptsCrawler kaipanlaConceptsCrawler) {
         this.tradeDateCrawler = tradeDateCrawler;
         this.wencaiConditionCrawler = wencaiConditionCrawler;
         this.emotionCrawler = emotionCrawler;
@@ -43,12 +43,12 @@ public class TradeDateController {
         this.kaipanlaTdCrawler = kaipanlaTdCrawler;
         this.kaipanlaConceptsCrawler = kaipanlaConceptsCrawler;
     }
-    
+
     @GetMapping("/init")
     public void init(@RequestParam("year") String year) {
         tradeDateCrawler.initTradeDate(year);
     }
-    
+
     @GetMapping("/wencai")
     public void wencai(@RequestParam("date") String date) {
         String[] dates = date.split(",");
@@ -56,24 +56,33 @@ public class TradeDateController {
             wencaiConditionCrawler.initSearchCondition(d);
         }
     }
-    
+
     @GetMapping("/emotion")
     public void emotion(@RequestParam("date") String date) {
         emotionCrawler.initEmotion(date);
     }
-    
+
     @GetMapping("/jiuyangongshe")
     public List jiuyangongshe(@RequestParam("date") String date) {
         return jiuyangongsheCrawler.initJiuyangongshe(date);
     }
-    
+
     @GetMapping("/kaipanla")
     public List kaipanla(@RequestParam("date") String date) {
         return kaipanlaTdCrawler.initKaipanlaTd(date);
     }
-    
+
     @GetMapping("/concepts")
     public void concepts(@RequestParam("date") String date) {
         kaipanlaConceptsCrawler.initKaipanlaConcepts(date);
+    }
+
+    @GetMapping("/crawler")
+    public void crawler(@RequestParam("date") String date) {
+        wencai(date);
+        emotion(date);
+        jiuyangongshe(date);
+        kaipanla(date);
+        concepts(date);
     }
 }
