469fef8c759e359e8b1a8b6af7742b5691474dac
wujm1 <wujm1@xiaopeng.com>
2025-04-08 17:54:42 +0800

    加点日志，提供健壮性

--- src/main/java/com/wujm1/tradesystem/crawler/emotiondata/EmotionCrawler.java
+++ src/main/java/com/wujm1/tradesystem/crawler/emotiondata/EmotionCrawler.java
@@ -10,6 +10,7 @@ import com.wujm1.tradesystem.mapper.ext.EmotionMapperExt;
 import com.wujm1.tradesystem.mapper.ext.TradeDateMapperExt;
 import com.wujm1.tradesystem.mapper.ext.WencaiConditionMapperExt;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.List;
@@ -35,18 +37,10 @@ import java.util.stream.Collectors;
 @Component
 @Slf4j
 public class EmotionCrawler {
-    /**
-     * 年度法定节假日、公休日查询接口地址
-     */
-    private static final String wencai_url = "https://www.iwencai.com/gateway/urp/v7/landing/getDataList?iwcpro=1";
 
     private final WencaiConditionMapperExt wencaiConditionMapperExt;
     private final EmotionMapperExt emotionMapperExt;
     private final TradeDateMapperExt tradeDateMapperExt;
-    /**
-     * http请求工具类
-     */
-    private static final RestTemplate restTemplate = new RestTemplate();
 
     public EmotionCrawler(WencaiConditionMapperExt wencaiConditionMapperExt, EmotionMapperExt emotionMapperExt, TradeDateMapperExt tradeDateMapperExt) {
         this.wencaiConditionMapperExt = wencaiConditionMapperExt;
@@ -55,31 +49,39 @@ public class EmotionCrawler {
     }
 
     public JSONObject crawler(String today2, String today1, String yesterday2) {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
         Map<String, WencaiCondition> wencaiConditionMap = wencaiConditionMapperExt.selectAll().stream().collect(Collectors.toMap(WencaiCondition::getConditionName, Function.identity()));
-        String cookies = wencaiConditionMap.get("cookies").getCondition();
-        String query = wencaiConditionMap.get("emotion_query").getCondition();
-        String condition = wencaiConditionMap.get("emotion_condition").getCondition();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.COOKIE, cookies);
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
-        params.add("query", query.replaceAll("today2", today2).replaceAll("today1", today1).replaceAll("yesterday2", yesterday2));
-        params.add("condition", condition.replaceAll("today2", today2).replaceAll("today1", today1).replaceAll("yesterday2", yesterday2));
-        params.add("urp_sort_index", "指数代码");
-        params.add("query_type", "zhishu");
-        params.add("source", "Ths_iwencai_Xuangu");
-        params.add("comp_id", "6829723");
-        params.add("uuid", "24089");
-        params.add("perpage", "100");
-        params.add("page", 1);
-        params.add("logid", "d99e29c29d58a269defdaa7d18ab7e5a");
-        params.add("ret", "json_all");
-        params.add("user_id", "Ths_iwencai_Xuangu_xqvhlculvxjnw4osr41up6c3al8um9q4");
-        params.add("business_cat", "soniu");
-        HttpEntity<MultiValueMap<String, Object>> httpEntity = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
-        ResponseEntity<String> responseEntity = restTemplate.postForEntity(wencai_url, httpEntity, String.class);
-        return JSON.parseObject(responseEntity.getBody());
+        String query = wencaiConditionMap.get("emotion_query").getCondition().replaceAll("today2", today2).replaceAll("today1", today1).replaceAll("yesterday2", yesterday2);
+        String condition = wencaiConditionMap.get("emotion_condition").getCondition().replaceAll("today2", today2).replaceAll("today1", today1).replaceAll("yesterday2", yesterday2);
+        String wencai_url = wencaiConditionMap.get("emtion_url").getCondition();
+        RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
+                .addFormDataPart("query", query)
+                .addFormDataPart("condition", condition)
+                .addFormDataPart("query_type", "stock")
+                .addFormDataPart("source", "Ths_iwencai_Xuangu")
+                .addFormDataPart("comp_id", "6836372")
+                .addFormDataPart("uuid", "24087")
+                .addFormDataPart("perpage", "100")
+                .addFormDataPart("page", "1")
+                .build();
+        Request request = new Request.Builder()
+                .url(wencai_url)
+                .method("POST", body)
+                .addHeader("Cookie", "other_uid=Ths_iwencai_Xuangu_xqvhlculvxjnw4osr41up6c3al8um9q4; ta_random_userid=ukoetmp31g; cid=61f00c6cae4404a7c5dc2a67ff81f10a1704935377; cid=61f00c6cae4404a7c5dc2a67ff81f10a1704935377; ComputerID=61f00c6cae4404a7c5dc2a67ff81f10a1704935377; WafStatus=0; guideState=1; user=MDpteF81MzIyMjI5MzE6Ok5vbmU6NTAwOjU0MjIyMjkzMTo3LDExMTExMTExMTExLDQwOzQ0LDExLDQwOzYsMSw0MDs1LDEsNDA7MSwxMDEsNDA7MiwxLDQwOzMsMSw0MDs1LDEsNDA7OCwwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSw0MDsxMDIsMSw0MDoxNjo6OjUzMjIyMjkzMToxNzA2MjU3ODcxOjo6MTU5NDM0NTY4MDoyNjc4NDAwOjA6MTY2YWU2ODM5ZWZjODQyMjZhZTI3OWQ0MTA4MDk5OGEwOmRlZmF1bHRfNDow; userid=532222931; u_name=mx_532222931; escapename=mx_532222931; ticket=db855d2ed9f1dc94d4de9afdd2711c3a; user_status=0; utk=06ab70f98e44c4a4806e032824c24f00; wencai_pc_version=1; v=A1JP610FYznzvp_FHb3x491aoxMxY1kJiElKaR2eeuzwcvyNBPOmDVj3moXv")
+                .build();
+        Response response = null;
+        try {
+            log.info("emotion query:{}  condition :{} ", query, condition);
+            response = client.newCall(request).execute();
+            String result = response.body().string();
+            log.info("emotion result:{}", result);
+            return JSON.parseObject(result);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
 
     public void initEmotion(String today2) {
         //获取最近两个交易日
@@ -91,7 +93,7 @@ public class EmotionCrawler {
         }
         List<TradeDate> tradeDateList = tradeDateMapperExt.selectByDateTopN(today2, 2);
         String yesterday2 = tradeDateList.get(0).getDate();
-        String today1 = tradeDateList.get(1).getYear() + "年" + tradeDateList.get(0).getMonth() + "月" + tradeDateList.get(0).getDay() + "日";
+        String today1 = tradeDateList.get(1).getYear() + "年" + tradeDateList.get(1).getMonth() + "月" + tradeDateList.get(1).getDay() + "日";
         JSONObject jsonObject = this.crawler(today2, today1, yesterday2);
         JSONArray datas = jsonObject.getJSONObject("answer").getJSONArray("components").getJSONObject(0)
                 .getJSONObject("data").getJSONArray("datas");
